7a7cf6d7df1ca9ce79e602dd5da01aadc6c9e8c6
Pratyush Hazarika <64987866+pratyushhz@users.noreply.github.com>
2021-07-07 20:03:11 +0530

    Update App.jsx

--- React/App.jsx
+++ React/App.jsx
@@ -5,17 +5,24 @@ const App = () => {
     const color = 'yellow';
     // destructuring / hook
     const [bg, updateBg] = useState(color);
+    const [name, updateName] = useState('Click me');
 
     // define bgChange event
     const bgColor = () => {
         let newBg = 'blue';
         updateBg(newBg);
+        updateName('Hello 👋');
     }
+     // define bgBack function:
+    const bgDoubleClick = () => {
+        updateBg(color);
+        updateName('Welcome you 😙 ');
+    };
         
     return(
         <>
             <div style= {{ backgroundColor: bg  }}>
-                <button onClick={bgColor}>Click me</button>
+                <button onClick={bgColor} onDoubleClick={bgDoubleClick}>{name}</button>
             </div>
         </>
     )
